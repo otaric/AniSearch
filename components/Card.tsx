7ce004d32e31abaaa04c55301dc8e75a3e48ceb8
William Marques <william.marques.pessoal@gmail.com>
2023-02-24 14:59:56 -0300

    responsividade dos cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,19 +12,17 @@ export default function Card({ title, image, link }: Props) {
       href={link}
       target="_blank"
       rel="noreferrer noopener"
-      className="group flex flex-col"
+      className="group flex max-w-[148px] flex-col sm:max-w-[185px] "
     >
       <div className="overflow-hidden text-neutral-50">
-        <div className="h-[265px] min-w-[185px]">
-          <img
-            className="h-full w-full rounded-lg object-cover object-center "
-            src={image}
-            alt={`Cover Image of ${title}`}
-          />
-        </div>
+        <img
+          className="h-[212px] w-[148px] rounded-lg object-cover object-center sm:h-[265px] sm:w-[185px] "
+          src={image}
+          alt={`Cover Image of ${title}`}
+        />
       </div>
 
-      <p className="w-full max-w-[192px] overflow-hidden text-ellipsis font-medium transition-all line-clamp-2 group-hover:text-rose-700">
+      <p className="w-full overflow-hidden text-ellipsis font-medium transition-all line-clamp-2 group-hover:text-rose-700">
         {title}
       </p>
     </a>
